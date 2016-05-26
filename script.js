0ebd3f2cea992485289b4e123c9e4a7aaf694b3a
Maxime Nialiv <maxime.vilain@gmx.fr>
2016-05-26 13:39:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say(`*Un robot inanimé posé sur une table* \n *Des larges yeux perplexes s'ouvrent à votre passage* \n Je suis le bot personnel de Maxime, un jeune-diplômé en communication et innovation, il m'a chargé de discuter à sa place. \n ![maxime](https://raw.githubusercontent.com/MaximeNialiv/EstherBot/master/img/maxime.jpg)`)
+            return bot.say(`Bonjour, je suis le bot personnel de Maxime, un jeune-diplômé en communication et innovation, il m'a chargé de discuter à sa place. \n ![maxime](https://raw.githubusercontent.com/MaximeNialiv/EstherBot/master/img/maxime.jpg)`)
                 .then(() => 'askName');
         }
     },
@@ -23,7 +23,8 @@ module.exports = new Script({
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(` ${name}... C'est un joli nom ! \n Enfin, c'est ce que l'on m'a dit de dire. Je ne saurais juger^^'\n Ma conversation est un peu laborieuse, vous ne pouvez me répondre qu'au moyen des mots en capitales, allez-y parlez BOT pour voir :) \n`))
+                .then(() => bot.say(` " ${name} "... C'est un joli nom ! \n Enfin, c'est ce que l'on m'a dit de dire. Je ne saurais juger^^'\n Je n'ai pas d'oreilles pour l'instant, du coup, il faut interagir avec les boutons. Si un bouton ne marche pas, il suffit d'écrire son intitulé pour l'actionner. \n Maxime m'a dit qu'il attendait une visite de la part de STORMZ %[STORMZ](postback:lol), vous venez pour cela ? \n Sinon on peut discuter de MAXIMEBOT %[MAXIMEBOT](postback:lol), les gens sont souvent intéressés par ma création. \n Ou alors on peut parler de MAXIME %[MAXIME](postback:lol), j'ai été créé pour lui trouver du boulot, mais vu qu'il en a trouvé un, je suis en train d'être reprogrammé... \n Du coup si tu veux savoir ce qu'il fait comme METIER %[METIER](postback:lol) et que veulent bien pouvoir dire GROWTH HACKING %[GROWTH HACKING](postback:lol) et UX DESIGN %[UX DESIGN](postback:lol), tu peux appuyer sur les boutons :) Je peux également te conter une histoire dont tu es le héros ou CYOP %[CYOP](postback:lol), pour Choose Your Ow Path ! Tu peux y aller, c'est plein de zombies et de chevaliers arthuriens :) ",
+`))
                 .then(() => 'speak');
         }
     },
