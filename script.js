43c967323b2efd7ccf95c2dad0269da87991dd8a
Maxime Nialiv <maxime.vilain@gmx.fr>
2016-06-16 22:37:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,17 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say(`Bonjour, je suis le bot personnel de Maxime, un jeune-diplômé en communication et innovation, il m'a chargé de discuter à sa place. \n ![maxime](https://raw.githubusercontent.com/MaximeNialiv/EstherBot/master/img/maxime.jpg)`)
-                .then(() => 'askName');
-        }
-    },
-
-    askName: {
-        prompt: (bot) => bot.say(`Puis-je connaître ton nom avant de commencer ?`),
-        receive: (bot, message) => {
-            const name = message.text;
-            return bot.setProp('name', name)
-                .then(() => bot.say(` " ${name} "... C'est un joli nom ! \n Enfin, c'est ce que l'on m'a dit de dire. Je ne saurais juger^^'\n Je n'ai pas d'oreilles pour l'instant, du coup, il faut interagir avec les boutons. Si un bouton ne marche pas, il suffit d'écrire son intitulé pour l'actionner. \n  On peut discuter de MAXIMEBOT %[MAXIMEBOT](postback:lol), les gens sont souvent intéressés par ma création. \n Ou alors on peut parler de MAXIME %[MAXIME](postback:lol), j'ai été créé pour lui trouver du boulot, mais vu qu'il en a trouvé un, je suis en train d'être reprogrammé... \n Du coup si tu veux savoir ce que veulent bien pouvoir dire GROWTH HACKING %[GROWTH HACKING](postback:lol) et UX DESIGN %[UX DESIGN](postback:lol), tu peux appuyer sur les boutons :) Je peux également te conter une histoire dont tu es le héros ou CYOP %[CYOP](postback:lol), pour Choose Your Ow Path ! Tu peux y aller, c'est plein de zombies et de chevaliers arthuriens :) `))
+            return bot.say(`Bonjour, tu viens discuter du bot de Bricool ? Pour le voir en action, il faut aller sur leur page Fb, pour discuter de ce que notre équipe Lion a fait et pensé, vous êtes au bon endroit ;) \n %[CONTINUE](postback:lol) `)
                 .then(() => 'speak');
         }
     },
